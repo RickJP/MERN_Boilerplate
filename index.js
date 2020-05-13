1dd2e2843457edfba19663093c3492c6a526af4e
rickjp <3741326+RickJP@users.noreply.github.com>
2020-05-13 14:44:32 +0900

    Setup login & register routes

--- index.js
+++ index.js
@@ -3,30 +3,59 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
-require('./db/db')
+require('./db/db');
 require('dotenv').config();
 
+const {User} = require('./models/user.js');
 
-const { User } = require('./models/user.js');
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 
-app.get('/', () => {
-  console.log('I am here!')
+
+const { auth } = require('./middleware/auth');
+
+app.get('/api/user/auth', auth, (req, res) => {
+  const { _id, email, name, lastName, role } = req.user;
+
+  res.json({
+    _id,
+    isAuth: true,
+    email,
+    name,
+    lastName,
+    role
+  })
 })
 
 app.post('/api/users/register', (req, res) => {
-  const user = new User(req.body)
+  const user = new User(req.body);
 
   user.save((err, userData) => {
-    if (err) return res.json({ success: false, error: err })
-    res.status(200).json({ success: true })
-  })
-  
-})
+    if (err) return res.json({success: false, error: err});
+    return res.json({success: true, data: userData});
+  });
+});
 
+app.post('/api/user/login', (req, res) => {
+  User.findOne({email: req.body.email}, (err, user) => {
+    if (!user) {
+      return res.json({loginSuccess: false, message: 'email not found'});
+    }
+
+    user.comparePasswords(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({loginSuccess: false, message: 'wrong password'});
+      }
+    });
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie('x_auth', user.token).status(200).json({
+        loginSuccess: true,
+      });
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: { 
@@ -29,6 +32,54 @@ const userSchema = mongoose.Schema({
     type:　Number,
   }
 })
+
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function( err, salt) {
+      if (err) return  next(err);
+  
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+          user.password = hash;
+          next();
+      })
+    })
+  } else {
+    next();
+  }  
+})
+
+userSchema.methods.comparePasswords = function(plainPw, cb) {
+  bcrypt.compare(plainPw, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+  const user = this;
+  const token = jwt.sign(user._id.toHexString(), 'secret')
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err)
+    cb(null, user);
+  })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+  const user = this;
+
+  jwt.verify(token, 'secret', function(err, decode) {
+    user.findOne({ _id: decode, "token": token }, function(err, user) {
+      if (err) return cb(err);
+      cb(null, user)
+    })
+  })
+}
 
 const User = mongoose.model('User', userSchema);
 
